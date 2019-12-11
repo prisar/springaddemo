e43f36da8342f985d268fa6c3f588b47159c47cc
prisar <pritimaysarkar2010@gmail.com>
2019-12-11 14:22:41 +0530

    integrate active directory with oauth

---
+++ src/main/java/com/rupeek/springaddemo/springaddemo/HelloController.java
@@ -0,0 +1,16 @@
+package com.rupeek.springaddemo.springaddemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   @Autowired
+   @PreAuthorize("hasRole('Users')")
+   @RequestMapping("/")
+   public String helloWorld() {
+      return "Hello World!";
+   }
+}
---
+++ src/test/java/com/rupeek/springaddemo/springaddemo/SpringaddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupeek.springaddemo.springaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
